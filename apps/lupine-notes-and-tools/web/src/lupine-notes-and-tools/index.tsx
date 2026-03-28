f00752c6af3173057e868b4966760ec539bba9f5
uuware <uuware@gmail.com>
2026-03-29 01:23:00 +1300

    Update docs

--- apps/lupine-notes-and-tools/web/src/lupine-notes-and-tools/index.tsx
+++ apps/lupine-notes-and-tools/web/src/lupine-notes-and-tools/index.tsx
@@ -17,6 +17,10 @@ import { themes } from '../styles/theme';
 import { baseCss } from '../styles/base-css';
 import { AppResponsiveFrame } from '../frames/app-responsive-frame';
 import { HomePage } from '../pages/home-page';
+import { FinancePage } from '../pages/finance-page';
+import { DiaryPage } from '../pages/diary-page';
+import { MinePage } from '../pages/mine-page';
+import { ToolsPage } from '../pages/tools-page';
 
 if (isFrontEnd() && webEnv('NODE_ENV', '') === 'development') {
   debugWatch(webEnv('API_PORT', 0));
@@ -30,6 +34,10 @@ setDefaultPageTitle('Lupine Notes and Tools');
 const pageRouter = new PageRouter();
 pageRouter.setFramePage({ component: AppResponsiveFrame, placeholderClassname: 'user-page-placeholder' });
 pageRouter.setSubDir('/lupine-notes-and-tools');
+pageRouter.use('/finance', FinancePage);
+pageRouter.use('/diary', DiaryPage);
+pageRouter.use('/mine', MinePage);
+pageRouter.use('/tools', ToolsPage);
 pageRouter.use('*', HomePage);
 
 bindRouter(pageRouter);

754b3d1376acefd41176a6da661310defbbd6785
uuware <uuware@gmail.com>
2026-03-29 01:14:25 +1300

    Add splash images

--- apps/lupine-notes-and-tools/web/capacitor.config.ts
+++ apps/lupine-notes-and-tools/web/capacitor.config.ts
@@ -2,8 +2,15 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.uuware.lupinenotesandtools',
-  appName: 'lupine-notes-and-tools-web',
+  appName: 'Lupine Notes',
   webDir: '../../../dist/server_root/lupine-notes-and-tools_web',
+  plugins: {
+    SplashScreen: {
+      backgroundColor: '#ffffff',
+      showSpinner: false,
+      launchFadeOutDuration: 300,
+    }
+  }
 };
 
 export default config;
